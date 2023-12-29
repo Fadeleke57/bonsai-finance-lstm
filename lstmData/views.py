28cbf58db1dcf45520d80a0d85eabdc382006e34
Farouk Adeleke <110058327+Fadeleke57@users.noreply.github.com>
2023-12-29 03:48:50 -0500

    formatting for views.py

--- lstmData/views.py
+++ lstmData/views.py
@@ -2,9 +2,6 @@ from django.utils import timezone
 from django.shortcuts import render
 from django.http import JsonResponse
 
-#from .models import lstmData
-#from .serializers import lstmDataSerializer
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,34 +10,7 @@ from .lstmModelDriver.driver import Driver
 from .lstmModelDriver.config import Configurations
 
 from datetime import datetime
-
-'''
-@api_view(['GET', 'POST'])
-def lstmData_list(request, format = None):
-
-    if request.method == 'GET':
-        data_points = lstmData.objects.all()
-        serializer = lstmDataSerializer(data_points, many=True)
-        return Response({
-            "status": "success",
-            "data": serializer.data,
-            "message": "List of stock predictions"
-        })
-    
-    if request.method == 'POST':
-        serializer = lstmDataSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response({
-                    "status": "success",
-                    "data": serializer.data,
-                    "message": "New stock prediction created"
-            }, status=status.HTTP_201_CREATED)
-        
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-'''  
+ 
 class StockPrediction:
     def __init__(self, stock_symbol, predicted_price):
         self.stock_symbol = stock_symbol
@@ -60,45 +30,22 @@ def lstmData_detail(request, ticker, format=None):
         configs = Configurations.get_configs(ticker)
         predictedPrice = Driver.get_price(configs)
 
-        if predictedPrice is None:
-            raise ValueError("Failed to predict price for the given ticker.")
-
-        # Create an instance of StockPrediction
+        # creates an instance of StockPrediction
         prediction = StockPrediction(ticker, predictedPrice)
 
-        # Prepare the JSON response
-        return Response({
-            "Response": {
-                "status": "success",
-                "data": prediction.to_json(),
-                "message": f"Next day stock price prediction for {ticker.upper()}"
-            }
-        })
+        # prepares the nested JSON response
+        response_data = {
+            "status": "success",
+            "data": prediction.to_json(),
+            "message": f"Next day stock price prediction for {ticker.upper()}"
+        }
+
+        return Response({"Response": response_data})
 
     except Exception as e:
-        return Response({
-            "Response": {
-                "status": "error",
-                "message": str(e) or "An error occurred while processing the request."
-            }  
-        }, status=status.HTTP_400_BAD_REQUEST)
+        error_response = {
+            "status": "error",
+            "message": str(e) or "An error occurred while processing the request."
+        }
+        return Response({"Response": error_response}, status=status.HTTP_400_BAD_REQUEST)
 
-"""  
-elif request.method == 'PUT':
-    serializer = lstmDataSerializer(ldata, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({
-            "status": "success",
-            "data": serializer.data,
-            "message": f"Stock prediction for {ldata.stockSymbol} updated"
-        })
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-elif request.method == 'DELETE':
-    lstmData.delete()
-    return Response({
-        "status": "success",
-        "message": "Stock prediction deleted"
-    }, status=status.HTTP_204_NO_CONTENT)
-"""      
