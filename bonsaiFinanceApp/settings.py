c39ca074c3804d198825e311b81fb34d36a87f6e
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-06-01 03:21:14 -0400

    envirnoment

--- bonsaiFinanceApp/settings.py
+++ bonsaiFinanceApp/settings.py
@@ -9,26 +9,25 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-APIKEY = os.getenv("API_KEY")
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+APIKEY = os.environ.get('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
-
+ALLOWED_HOSTS = ['https://bonsaifinance.onrender.com', '127.0.0.1']
 
 # Application definition
 INTERNAL_IPS = [
